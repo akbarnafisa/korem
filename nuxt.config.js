b0a79d86e74f5e73df1a075965872db03e43ac0f
hdriqi <ra@evius.id>
2019-03-27 00:16:25 +0700

    update desc

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: `Website resmi Korem 064 Maulana Yusuf Serang Banten` }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'https://gateway.serph.network/ipfs/QmdGL3uVQZ3RmenLkm3S9JdEyHsj6bWqFHh1PPwcamJiu4' },
